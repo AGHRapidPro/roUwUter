fa3f32c3e271edd0ec0891dcd7ffe1e7998c3325
anna <annaniezgoda@gmailc.om>
2026-03-10 23:07:00 +0100

    rouwuter

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import os
 from dotenv import load_dotenv
 import requests
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 load_dotenv()
 url = os.environ.get("URL")
-user = os.environ.get("USER")
+user = os.environ.get("USERNAME")
 passwd = os.environ.get("PASS")
 
-print(url)
-
-@app.route('/api/devices')
-def get_data():
+@app.route('/')
+def index():
     r = requests.get(url, auth=(user, passwd), verify=False)
-    return r.json()
-
+    devices = r.json()
+    return render_template("index.html", devices=devices)
