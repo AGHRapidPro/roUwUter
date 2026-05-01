36388157c86dd4e54ea20afe21792020df1f9e03
Sivaj <48877780+sivajone@users.noreply.github.com>
2026-05-01 17:54:08 +0200

    fix miau

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from turbo_flask import Turbo
 import threading
 import time
 from mac_vendor_lookup import MacLookup
+import urllib3
+
+urllib3.disable_warnings()
 
 mac = MacLookup()
 mac.update_vendors()
@@ -13,38 +16,20 @@ mac.update_vendors()
 
 load_dotenv()
 url = os.environ.get("URL")
-user = os.environ.get("USERNAME")
-passwd = os.environ.get("PASS")
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def index():
-#     r = requests.get(url, auth=(user, passwd), verify=False)
-#     devices = r.json()
-#     for device in devices:
-#         if device['status'] == 'bound':
-#             try:
-#                 device['vendor'] = mac.lookup(device["active-mac-address"])
-#             except:
-#                 #print(device['active-mac-address'])
-#                 device['vendor'] = 'NaN😜'
-#     return render_template("index.html", devices=devices)
-
+user = os.environ.get("API_USER")
+passwd = os.environ.get("API_PASS")
 
 app = Flask(__name__)
 turbo = Turbo(app)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.context_processor
 def inject_data():
-    # r = requests.get("https://timeapi.io/api/v1/time/current/unix")
-    # data = r.json()
-    # return {'data': data}
     r = requests.get(url, auth=(user, passwd), verify=False)
     devices = r.json()
     for device in devices:
@@ -53,17 +38,21 @@ def inject_data():
                 device['vendor'] = mac.lookup(device["active-mac-address"])
             except:
                 device['vendor'] = 'NaN😜'
-            # if device['hostname'] == None:
-                # device['hostname'] = '-'
     return {'devices': devices}
+
+thread_started = False
+@app.before_request
+def before_first_request():
+    global thread_started
+    if not thread_started:
+        threading.Thread(target=update_data, daemon=True).start()
+        thread_started = True
 
 
 def update_data():
     with app.app_context():
         while True:
-            time.sleep(5)
-            turbo.push(turbo.replace(render_template('index.html'), 'devices'))
-
-@app.before_request
-def before_first_request():
-    threading.Thread(target=update_data).start()
+            time.sleep(10)
+            print("replacing")
+            turbo.push(turbo.replace(
+                render_template('devices.html'), 'devices'))
